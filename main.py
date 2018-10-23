950567c66bf4d39db8ddc576b994fdab3e30f052
RompelSt0mpel <pvvuuren@live.co.za>
2018-10-23 19:25:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -11,19 +11,10 @@ PREFIX = ">"
 bot = commands.Bot(command_prefix=PREFIX, self_bot=True)
 
 
-@bot.command(pass_context=True, aliases=['g'])
-async def game(ctx, args):
-
-    if args:
-        cstatus = ctx.message.server.get_member(bot.user.id).status
-        await bot.change_presence(game=Game(name=txt), status=cstatus)
-        msg = await bot.send_message(ctx.message.channel, embed=Embed(color=Color.green(), description="Changed game to `%s`!" % txt))
-    else:
-        await bot.change_presence(game=None, status=cstatus)
-        msg = await bot.send_message(ctx.message.channel, embed=Embed(color=Color.gold(), description="Disabled game display."))
-    await bot.delete_message(ctx.message)
-    await asyncio.sleep(3)
-    await bot.delete_message(msg)
+async def game(ctx, *args):
+		await client.wait_until_ready()
+		await client.change_presence(game=discord.Game(name="Cloak & Dagger"))
+		await asyncio.sleep(3)
 
 
 @bot.command(pass_context=True, aliases=['s'])
