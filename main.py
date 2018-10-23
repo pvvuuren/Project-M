459a1898e6daa5fe02bcb01183e662a4271a6c5b
RompelSt0mpel <pvvuuren@live.co.za>
2018-10-23 19:43:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import os
 import discord
 import asyncio
+import random
+from messages import messages_list
+from copy import deepcopy
 
 key = os.environ['TOKEN']
 
@@ -9,7 +12,17 @@ client = discord.Client()
 async def background_loop():
     await client.wait_until_ready()
     await client.change_presence(game=discord.Game(name="Watching you"))
-	await asyncio.sleep(21600)
+    last_message = None
+    messages = deepcopy(messages_list)
+    cleanmessages = deepcopy(messages_list)
+    while not client.is_closed:
+        channel = client.get_channel("242956769503084544")
+        if not messages:
+            messages = deepcopy(cleanmessages)
+        if len([x async for x in client.logs_from(channel, limit=2, after=last_message)]) > 1:
+            content = messages.pop(random.randrange(0, len(messages)))
+            last_message = await client.send_message(channel, content)
+            await asyncio.sleep(21600)
 
 client.loop.create_task(background_loop())
 client.run(key)
