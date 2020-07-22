3a6906bfb59bc1a4a71dd5f09f21306cd42acac1
RompelSt0mpel <32298792+pvvuuren@users.noreply.github.com>
2020-07-22 17:02:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,37 @@ import os
 import discord
 import asyncio
 import giphypop
-#from discord import game, status
 from discord.ext import commands
 
 key = os.environ['TOKEN']
+PREFIX = ">"
 
-# Creating selfbot instance
 bot = commands.Bot(command_prefix=PREFIX, self_bot=True)
 
 @bot.command(pass_context=True, aliases=['s'])
-
+async def status(ctx, *args):
+    stati = {
+        "on":       Status.online,
+        "online":   Status.online,
+        "off":      Status.invisible,
+        "offline":  Status.invisible,
+        "dnd":      Status.dnd,
+        "idle":     Status.idle,
+        "afk":      Status.idle
+    }
+    if args:
+        cgame = ctx.message.server.get_member(bot.user.id).game
+        if (args[0] in stati):
+            if (args[0] == "afk"):
+                await bot.change_presence(game=cgame, status=Status.idle, afk=True)
+            else:
+                await bot.change_presence(game=cgame, status=stati[args[0]], afk=False)
+                print(stati[args[0]])
+            msg = await bot.send_message(ctx.message.channel, embed=Embed(description="Changed current status to `%s`." % args[0], color=Color.gold()))
+    else:
+        await bot.change_presence(game=cgame, status=Status.online, afk=False)
+        msg = await bot.send_message(ctx.message.channel, embed=Embed(description="Changed current status to `online`.", color=Color.gold()))
+    await bot.delete_message(ctx.message)
+    await asyncio.sleep(3)
+    await bot.delete_message(msg)
 bot.run(key, bot=False)
