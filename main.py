429c29b2d37e1561026a282b5bfb9e33f5360d4a
RompelSt0mpel <pvvuuren@live.co.za>
2020-07-22 17:48:47 +0200

    Update main.py
    Added workaround

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
+--- channel.py.old  2017-02-27 15:02:23.000000000 -0800
++++ channel.py  2020-07-22 02:44:03.000000000 -0700
+@@ -27,13 +27,28 @@
+ from . import utils
+ from .permissions import Permissions, PermissionOverwrite
+ from .enums import ChannelType
+-from collections import namedtuple
+ from .mixins import Hashable
+ from .role import Role
+ from .user import User
+ from .member import Member
 import os
 import discord
 import asyncio
 import giphypop
-#from discord import game, status
+
 from discord.ext import commands
+ 
+-Overwrites = namedtuple('Overwrites', 'id allow deny type')
++class Overwrites:
++    __slots__ = ('id', 'allow', 'deny', 'type')
++
++    def __init__(self, **kwargs):
++        self.id = kwargs.pop('id')
++        self.allow = kwargs.pop('allow', 0)
++        self.deny = kwargs.pop('deny', 0)
++        self.type = kwargs.pop('type')
++
++    def _asdict(self):
++        return {
++            'id': self.id,
++            'allow': self.allow,
++            'deny': self.deny,
++            'type': self.type,
++        }
++
+ 
+ class Channel(Hashable):
 
 key = os.environ['TOKEN']
-# Default command prefix is set to '>', change it here if you want
 PREFIX = ">"
 
 # Creating selfbot instance
