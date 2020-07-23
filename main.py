7d22de58d7b03c4afe34c383dca756c88fac0bef
RompelSt0mpel <pvvuuren@live.co.za>
2020-07-23 14:37:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,15 +6,8 @@ key = os.environ['TOKEN']
 client = discord.Client()
 
 @client.event
-async def on_ready():
-    print("Logged in as")
-    print(client.user.name)
-    print("------")
-    # To start background tasks
-    game_info.start()
-
 async def game_info():
-    activity = discord.Activity(name="testing", type=discord.ActivityType.playing)
+    activity = discord.Activity(name="I am watching", type=discord.ActivityType.playing)
     await bot.change_presence(activity=activity)
 	
 	
