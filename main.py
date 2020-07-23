3ad104deae1ebc314513f42e6d65cd72309ebfe9
RompelSt0mpel <pvvuuren@live.co.za>
2020-07-23 13:18:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 import discord
+import asyncio
+import giphypop
+from discord import game, status
 
 key = os.environ['TOKEN']
 
@@ -7,5 +10,6 @@ client = discord.Client()
 
 async def background_loop():
     await client.change_presence(game=discord.Game(name="testing"))
+	await asyncio.sleep(3)
 
-client.run(key)
+client.run(key, bot=False)
